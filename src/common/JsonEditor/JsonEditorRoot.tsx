eddc87082dceb7063b48edeac6406aa1231c6066
fwl <admin@formidableworks.com>
2022-08-12 08:19:47 +0100

    implemented confirm dialog.

--- src/common/JsonEditor/JsonEditorRoot.tsx
+++ src/common/JsonEditor/JsonEditorRoot.tsx
@@ -1,14 +1,16 @@
 import Editor, { loader, Monaco, OnValidate } from '@monaco-editor/react';
-import { Grid, useTheme } from '@mui/material';
+import { Grid, GridProps, styled, useTheme } from '@mui/material';
 import * as monacoEditor from 'monaco-editor';
 import { editor as editorApi } from 'monaco-editor/esm/vs/editor/editor.api';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { DeliveryConfigToolbar } from '../../dashboard/DeliveryConfigToolbar';
+import { ConfirmDialog } from './ConfirmDialog';
 import { deliveryConfigMonacoSchema } from './DeliveryConfigSchema';
 import { IssuesIndicator } from './IssuesIndicator';
 import { KeyboardShortcuts } from './KeyboardShortcuts';
+import { cobaltTheme } from './monacoThemes';
 
 // expose types from namespace.
 type IStandaloneCodeEditor = editorApi.IStandaloneCodeEditor;
@@ -26,11 +28,63 @@ self.MonacoEnvironment = {
 // ensure monaco components are loaded from node_modules not a cdn.
 loader.config({ monaco: monacoEditor });
 
-export function JsonEditorRoot(): JSX.Element {
+interface EditorWrapperProps extends GridProps {
+  isFocused: boolean;
+}
+const EditorWrapper = styled(Grid, {
+  shouldForwardProp: (prop) => prop !== 'isFocused',
+})<EditorWrapperProps>(({ isFocused, theme }) => ({
+  pl: 0,
+  width: '100%',
+  borderWidth: 2,
+  borderStyle: 'solid',
+  borderColor: theme.palette.divider,
+  ...(isFocused && {
+    borderColor:
+      theme.palette.mode === 'dark' ? theme.palette.primary.dark : theme.palette.primary.light,
+  }),
+}));
+
+interface StatusBarProps extends GridProps {
+  isFocused: boolean;
+}
+const StatusBar = styled(Grid, {
+  shouldForwardProp: (prop) => prop !== 'isFocused',
+})<StatusBarProps>(({ isFocused, theme }) => ({
+  paddingLeft: theme.spacing(1),
+  paddingRight: theme.spacing(1),
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '100%',
+  borderBottomLeftRadius: theme.spacing(1),
+  borderBottomRightRadius: theme.spacing(1),
+  borderWidth: 2,
+  borderTopWidth: 0,
+  borderStyle: 'solid',
+  borderColor: theme.palette.divider,
+  ...(isFocused && {
+    borderColor:
+      theme.palette.mode === 'dark' ? theme.palette.primary.dark : theme.palette.primary.light,
+  }),
+}));
+
+interface Props {
+  value: string;
+  onSave: (value: string) => void;
+}
+export function JsonEditorRoot(props: Props): JSX.Element {
+  const { value, onSave } = props;
+  const [editorValue, setEditorValue] = useState<string | undefined>(value);
   const [isFocused, setFocused] = useState(false);
+  const [isConfirmDialogOpen, setConfirmDialogOpen] = useState(false);
   const [editorMarkers, setEditorMarkers] = useState<IMarker[]>([]);
   const theme = useTheme();
   const editorRef = useRef<IStandaloneCodeEditor | null>(null);
+
+  // only update internal prop-derived-state when prop changes.
+  useEffect(() => {
+    setEditorValue(value);
+  }, [value]);
 
   const editorOnMount = (editor: IStandaloneCodeEditor, monaco: Monaco) => {
     editorRef.current = editor;
@@ -44,13 +98,15 @@ export function JsonEditorRoot(): JSX.Element {
     // use monaco's focus/blur listeners to set isFocused appropriately.
     editor.onDidFocusEditorText(() => setFocused(true));
     editor.onDidBlurEditorText(() => setFocused(false));
+    // add cobalt theme (closer match to dark theme).
+    monaco.editor.defineTheme('cobalt', cobaltTheme);
   };
 
   const handleFormat = () => {
     editorRef.current?.trigger('external toolbar', 'editor.action.formatDocument', undefined);
   };
   const handleSave = () => {
-    console.log('save');
+    setConfirmDialogOpen(true);
   };
   const handleValidate: OnValidate = (markers) => {
     setEditorMarkers(markers);
@@ -64,82 +120,39 @@ export function JsonEditorRoot(): JSX.Element {
     });
     editorRef.current?.revealLine(marker.startLineNumber);
   };
+
   return (
     <>
       <DeliveryConfigToolbar onFormat={handleFormat} onSave={handleSave} />
-      <Grid
+      <EditorWrapper
         item
-        xs={12}
-        sm={12}
-        md={12}
-        lg={12}
-        xl={12}
-        sx={(t) => ({
-          pl: 0,
-          width: '100%',
-          borderWidth: 1,
-          borderStyle: 'solid',
-          borderColor: t.palette.divider,
-          ...(isFocused && {
-            borderColor:
-              t.palette.mode === 'dark' ? t.palette.primary.dark : t.palette.primary.light,
-          }),
-        })}
+        isFocused={isFocused}
+        columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}
       >
         <Editor
           onMount={editorOnMount}
           options={{ minimap: { enabled: false } }}
-          theme={theme.palette.mode === 'dark' ? 'vs-dark' : 'light'}
+          theme={theme.palette.mode === 'dark' ? 'cobalt' : 'light'}
           height="15rem"
           defaultLanguage="json"
-          defaultValue={`
-          {
-            "signature_required": "yes indeed", "customer_id": "1235339", "service_tier": "invalid enum val",
-            "address": {
-              "addressee": "Stringer Bell",
-              "premise_identifier": "Barksdale towers",
-            },
-            "handling_tags": [
-              "fragile",
-              "fragile"
-            ],
-            "dimensions": {
-              "width": -1,
-              "height": 4,
-              "depth": 1000
-            }
-          }
-          `}
+          value={editorValue}
+          onChange={(val) => setEditorValue(val)}
           onValidate={handleValidate}
         />
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={12}
-        md={12}
-        lg={12}
-        xl={12}
-        sx={(t) => ({
-          pl: 2,
-          pr: 1,
-          display: 'flex',
-          justifyContent: 'space-between',
-          width: '100%',
-          borderBottomLeftRadius: t.spacing(1),
-          borderBottomRightRadius: t.spacing(1),
-          borderWidth: 1,
-          borderStyle: 'solid',
-          borderColor: t.palette.divider,
-          ...(isFocused && {
-            borderColor:
-              t.palette.mode === 'dark' ? t.palette.primary.dark : t.palette.primary.light,
-          }),
-        })}
-      >
+      </EditorWrapper>
+      <StatusBar item columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }} isFocused={isFocused}>
         <KeyboardShortcuts />
         <IssuesIndicator markers={editorMarkers} onMarkerClick={handleMarkerClick} />
-      </Grid>
+      </StatusBar>
+      <ConfirmDialog
+        isOpen={isConfirmDialogOpen}
+        onClose={() => setConfirmDialogOpen(false)}
+        originalValue={value}
+        modifiedValue={editorValue}
+        onConfirm={() => {
+          onSave(editorValue ?? '');
+        }}
+      />
     </>
   );
 }
